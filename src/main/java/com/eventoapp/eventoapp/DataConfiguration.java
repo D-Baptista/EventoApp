c195a9575bc74f1510dce236ab59e3a2e6c7963a
D. Baptista <117957767+D-Baptista@users.noreply.github.com>
2026-01-11 18:54:22 +0100

    Update DataBaseConfiguration File

--- src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -1,34 +1,24 @@
-package com.eventoapp.eventoapp;
-
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
 @Configuration
 public class DataConfiguration {
     
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dSource = new DriverManagerDataSource();
-        dSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dSource.setUrl("jdbc:mysql://localhost:3306/eventoapp");
-        dSource.setUsername("root");
-        dSource.setPassword("");
+        // Usa variáveis de ambiente ou valores padrão (para rodar local)
+        dSource.setDriverClassName("org.postgresql.Driver"); 
+        dSource.setUrl(System.getenv("SPRING_DATASOURCE_URL")); 
+        dSource.setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
+        dSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
         return dSource;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabase(Database.POSTGRESQL); // Mudar de MYSQL para POSTGRESQL
         adapter.setShowSql(true);
         adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
         adapter.setPrepareConnection(true);
         return adapter;
     }
